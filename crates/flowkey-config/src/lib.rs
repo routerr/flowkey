fea97987e771ea2e9b258bef4786663d6223943b
routerr <saintanorpice@gmail.com>
2026-04-21 00:28:16 +0800

    Debug for build error

--- crates/flowkey-config/src/lib.rs
+++ crates/flowkey-config/src/lib.rs
@@ -1,3 +1,6 @@
 mod config;
 
-pub use config::{unix_timestamp_now, CaptureMode, Config, NodeConfig, PeerConfig, SwitchConfig};
+pub use config::{
+    unix_timestamp_now, CaptureMode, Config, DEFAULT_INPUT_COALESCE_WINDOW_MS, NodeConfig,
+    PeerConfig, SwitchConfig,
+};
